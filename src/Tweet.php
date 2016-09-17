f1ee1d6d0b92ee7edf3beb742a8651ee597eb9b4
jaczkowska <kjaczkowska@gmail.com>
2016-09-17 16:46:22 +0200

    added setters and getters

--- src/Tweet.php
+++ src/Tweet.php
@@ -14,4 +14,33 @@ class Tweet {
         $this->creationDate = 0;
     }
 
+    function getId() {
+        return $this->id;
+    }
+
+    function getUserId() {
+        return $this->userId;
+    }
+
+    function getText() {
+        return $this->text;
+    }
+
+    function getCreationDate() {
+        return $this->creationDate;
+    }
+
+    function setUserId($userId) {
+        $this->userId = $userId;
+    }
+
+    function setText($text) {
+        $this->text = $text;
+    }
+
+    function setCreationDate($newDate) {
+        $this->creationDate = $newDate;
+    }
+
+
 }
