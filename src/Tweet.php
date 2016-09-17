04a8dfe539eee1dfeae0419a38914c7eca80b3b4
jaczkowska <kjaczkowska@gmail.com>
2016-09-17 16:55:54 +0200

    added static function LOAD one Tweet and all tweets from db

--- src/Tweet.php
+++ src/Tweet.php
@@ -42,5 +42,41 @@ class Tweet {
         $this->creationDate = $newDate;
     }
 
+    //LOAD Tweet from db
+    static public function loadTweetById(mysqli $connection, $id) {
+        $db = "SELECT * FROM Tweet WHERE id=$id";
+        $result = $connection->query($db);
+        if ($result && $result->num_rows == 1) {
+            $row = $result->fetch_assoc();
+            $loadTweet = new Tweet();
+            $loadTweet->id = $row['id'];
+            $loadTweet->userId = $row['user_id'];
+            $loadTweet->text = $row['text'];
+            $loadTweet->creationDate = $row['creation_date'];
+            return $loadTweet;
+        }
+    }
 
+    //LOAD ALL Tweets from db
+    static public function loadAllTweetsByUserId(mysqli $connection, $userId) {
+
+        $db = "SELECT * FROM Tweet WHERE user_id = $userId ORDER BY creation_date DESC";
+        $result = $connection->query($db);
+
+        $allTweets = [];
+        if ($result && $result->num_rows != 0) {
+            foreach ($result as $row) {
+                $loadedTweet = new Tweet();
+                $loadedTweet->id = $row['id'];
+                $loadedTweet->userId = $row['user_id'];
+                $loadedTweet->text = $row['text'];
+                $loadedTweet->creationDate = $row['creation_date'];
+                $allTweets[] = $loadedTweet;
+            }
+        }
+
+        return $allTweets;
+    }
+
+    
 }
