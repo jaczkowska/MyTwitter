a925e5094c9456c65ee7d60c775dc83f23959042
jaczkowska <kjaczkowska@gmail.com>
2016-09-18 20:52:15 +0200

    change a href...

--- index.php
+++ index.php
@@ -4,7 +4,20 @@ require_once 'src/init.php';
 require_once 'src/Users.php';
 require_once 'src/Tweet.php';
 
+//Check if user is logged in
+if (!isset($_SESSION['loggedUserId'])) {
 
+    header('Location: loginRegister.php');
+    
+} else {
+
+    $loggedUserId = $_SESSION['loggedUserId'];
+    $loggedUser = Users::loadUserById($conn, $loggedUserId);
+    
+}
+
+
+//TWEETS
 if ($_SERVER['REQUEST_METHOD'] === 'POST' &&
         isset($_POST['text'])) {
     $newTweet = new Tweet();
@@ -54,25 +67,29 @@ $allTweets = Tweet::loadAllTweets($conn);
         <div class="container">
             <div class="row">
                 <div class="col-sm-offset-2 col-sm-8 main paddingMainBG">
-                        <?php
-                        foreach ($allTweets as $tweet) {
-                            $tweetId = $tweet->getId();
-                            $userId = $tweet->getUserId();
-                            $username = User::loadUserById($conn, $userId)->getUsername();
-                            $creationDate = $tweet->getCreationDate();
-                            $text = $tweet->getText();
-                            
-                            echo "<div class='form-group' >
-                                <h5><a href='userInfo.php?id=$userId'>$username>/a></h5>
-                                <span><a href='tweetInfo.php?id=$tweetId'>$creationDate</a></span>
-                                <input class='form-control input' type='textarea' placeholder='$text'>
-                                <button type='submit' class'btn btn-success col-sm-2'><span class='glyphicon glyphicon-send'> Tweet!</span></button>
-                            </div>";
-                        }
-                        ?>
-                    </div>
+                    <?php
+                    foreach ($allTweets as $tweet) {
+                        $tweetId = $tweet->getId();
+                        $userId = $tweet->getUserId();
+                        $username = Users::loadUserById($conn, $userId)->getUsername();
+                        $creationDate = $tweet->getCreationDate();
+                        $text = $tweet->getText();
+
+                        echo "<div class='form-group' >
+                                <h5><span class='glyphicon glyphicon-user'><a href='userPage.php?id=$userId'> $username</a></span></h5>
+                                <span class='glyphicon glyphicon-globe'><a href='tweetDetails.php?id=$tweetId'> $creationDate</a></span>
+                                <input class='form-control input' type='textarea' placeholder='$text' disabled>
+                                <button type='submit' class='btn btn-success col-sm-3 inline'><span class='glyphicon glyphicon-pencil'><a href=''> Edit tweet</a></span></button>
+                                <button type='submit' class='btn btn-danger col-sm-3 inline'><span class='glyphicon glyphicon-trash'><a href=''> Delete tweet</a></span></button>
+                                <button type='submit' class='btn btn-primary col-sm-3 inline'><span class='glyphicon glyphicon-comment'><a href=''> Comment</a></span></button>
+                                <br>
+                                <hr>
+                            </div>";  
+                    }
+                    ?>
                 </div>
             </div>
+        </div>
 
         <!-- FOOTER -->
         <?php require_once 'layouts/footer.php'; ?>
